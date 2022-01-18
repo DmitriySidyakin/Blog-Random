29807e7619921002e7ac98d9c117e2d6e1c930ad
Dmitriy Sidyakin <sidyakind@gmail.com>
2022-01-18 20:54:05 +0300

    Обновил RandomComputationalMethods.cs и тесты
    Добавил полную реализацию класса RandomComputationalMethods

---
+++ Blog-Random/Program.cs
@@ -0,0 +1,68 @@
+﻿using Blog_Random.Random;
+using System;
+using System.Collections.Generic;
+
+namespace Blog_Random
+{
+    public class Program
+    {
+        public static long GenerationCount { get; } = 1000;
+
+        public static long Min { get; } = byte.MinValue;
+
+        public static long Max { get; } = byte.MaxValue;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Random generator tests.");
+            Console.WriteLine("_______________________");
+            Console.WriteLine("Test 1: Generator RandomComputationalMethods");
+            Test01();
+
+            Console.ReadLine();
+        }
+
+        static void Test01()
+        {
+            RandomComputationalMethods randomGenerator = new RandomComputationalMethods(5123);
+            List<byte> rnds = new List<byte>();
+
+            for (long i = 0; i < GenerationCount; i++)
+            {
+
+                //Console.WriteLine(randomGenerator.RandomDoubleBetweenOne());
+                Console.WriteLine(randomGenerator.RandomLongBetweenInt(0, 100));
+
+                /*
+                DateTime start = DateTime.Now;
+                while (randomGenerator.RandomLongBetween(0, 100) != 100) ;
+                DateTime end = DateTime.Now;
+                Console.WriteLine($"Ticks = {end.Ticks - start.Ticks}");
+                */
+                /*
+                byte rnd = randomGenerator.RandomByte();
+
+                if(!rnds.Contains(rnd))
+                {
+                    rnds.Add(rnd);
+                }
+                Console.WriteLine($"rnd = {rnd}");
+                if (rnd < Min || rnd > Max)
+                {
+                    Console.WriteLine($"It Have been found a error! rnd = {rnd}");
+                }
+                if(rnd == Min || rnd == Max)
+                {
+                    Console.WriteLine($"Edge rnd = {rnd}");
+                }*/
+            }
+            /*
+            double fraction;
+            while((fraction = randomGenerator.RandomDoubleBetweenOne()) != 1.0) ;
+            Console.WriteLine(fraction);*/
+
+            //Console.WriteLine($"rnds.Count={rnds.Count}");
+        }
+    }
+}
